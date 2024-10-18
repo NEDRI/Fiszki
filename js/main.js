81d3186bac20bb8ce0b9cbd4c28d10eb694cb090
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-10-18 10:17:49 +0200

    Cos poprawilem

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const okinko = document.getElementById("okinko");
+    const box = document.getElementById("box");
 
-    okinko.style.opacity = "0";
-    okinko.style.transform = "scale(0.7)";
+    box.style.opacity = "0";
+    box.style.transform = "scale(0.7)";
 
     setTimeout(() => {
-        okinko.style.transition = "opacity 0.8s ease, transform 0.8s ease";
-        okinko.style.opacity = "1";
-        okinko.style.transform = "scale(1)";
+        box.style.transition = "opacity 0.8s ease, transform 0.8s ease";
+        box.style.opacity = "1";
+        box.style.transform = "scale(1)";
     }, 200);
 });
